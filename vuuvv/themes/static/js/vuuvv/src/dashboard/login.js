4d78de3863a2d38e3705d696522ab2064e29d4e6
unknown <yxzxzy@.joyou.com>
2012-12-15 11:41:16 +0800

    login panel

--- vuuvv/themes/static/js/vuuvv/src/dashboard/login.js
+++ vuuvv/themes/static/js/vuuvv/src/dashboard/login.js
@@ -6,6 +6,26 @@ require([
 	"dojo/NodeList-manipulate",
 	"dojo/domReady!"
 ], function(query) {
+
+	var is_visible = function(nl) {
+		if (nl.length < 1)
+			return false;
+
+		var node = nl[0], p
+
+		if (node.domNode) {
+			node = node.domNode;
+		}
+
+		return (dojo.style(node, "display") != "none") &&
+			   (dojo.style(node, "visibility") != "hidden") &&
+			   (p = dojo.position(node), p.y + p.h >= 0 && p.x + p.w >= 0 && p.h && p.w);
+	}
+
+	var is_hidden = function(nl) {
+		return !is_visible();
+	}
+
 	var inputs = query(".textbox input").data("empty", true);
 	inputs.on("focus", function(e) {
 		var input = query(this);
@@ -26,6 +46,30 @@ require([
 		if (!input.val() || input.val() == input.attr('tip')) {
 			input.val(input.attr("tip"));
 			box.addClass("textbox-tip");
+		}
+	});
+	inputs.on("keyup", function(e) {
+		var input = query(this);
+		var box = input.parent();
+
+		if (e.keyCode == 13) {
+			if (input.filter("#user_name").length > 0) {
+				query("#password")[0].focus();
+			} else if (input.filter("#password").length > 0) {
+				var vcode = $("#vcode");
+				if (is_hidden(vcode)) {
+					vcode[0].focus();
+				} else {
+					query("#form_login")[0].submit();
+				}
+			} else if (input.filter("#vcode")) {
+				query("#form_login")[0].submit();
+			}
+		}
+	});
+	inputs.on("keydown", function(e) {
+		if (e.keyCode == 13) {
+			e.preventDefault();
 		}
 	});
 	inputs[0].focus();
