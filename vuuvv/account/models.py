971c12bfb04013ed06ce91463489c4bf03a3cda3
unknown <yxzxzy@.joyou.com>
2012-12-20 20:52:10 +0800

    account

---
+++ vuuvv/account/models.py
@@ -0,0 +1,26 @@
+import datetime
+from flask import url_for
+from vuuvv import db
+
+METHOD = ('GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'TRACE', 'ALL')
+
+class Permission(db.EmbeddedDocument):
+    view = db.StringField(max_length=255, required=True)
+    methods = db.ListField(
+        db.StringField(max_length=10, required=True, choices=METHOD),
+        default=['GET']
+    )
+
+class Role(db.Document):
+    name = db.StringField(max_length=255, required=True)
+    permissions = db.ListField(db.EmbeddedDocumentField('Permission'))
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+
+class User(db.Document):
+    username = db.StringField(max_length=255, required=True)
+    password = db.StringField(max_length=255, required=True)
+    email = db.StringField(max_length=255, required=True)
+    permissions = db.ListField(db.EmbeddedDocumentField(Permission))
+    roles = db.ListField(db.ReferenceField(Role))
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+
--- vuuvv/manage.py
+++ vuuvv/manage.py
@@ -1,5 +1,4 @@
 import os, sys
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
@@ -25,14 +24,28 @@ manager.add_command("runserver", Server(
     host = '0.0.0.0')
 )
 
+@manager.option('-t', '--host', dest='host', default='0.0.0.0')
+@manager.option('-p', '--port', dest='port', type=int, default=5000)
+@manager.option('--threaded', dest='threaded', action='store_true', default=False)
+@manager.option('--processes', dest='processes', type=int, default=1)
+@manager.option('--passthrough-errors', dest='passthrough_errors', action='store_true', default=False)
 @manager.option('-m', '--mode', dest='mode', default='dev')
-def startserver(mode):
+def startserver(mode, host, port, threaded, processes, passthrough_errors):
     config_app(modes[mode])
-    server = Server(
-        use_debugger = True,
-        use_reloader = True,
-        host = '0.0.0.0')
+    use_debugger = False
+    use_reloader = False
+    if mode == 'dev':
+        use_debugger = True
+        use_reloader = True
+
+    app.run(host=host,
+            port=port,
+            debug=app.config.get('DEBUG', use_debugger),
+            use_debugger=use_debugger,
+            use_reloader=use_reloader,
+            threaded=threaded,
+            processes=processes,
+            passthrough_errors=passthrough_errors)
 
 if __name__ == '__main__':
-    import pdb;pdb.set_trace()
     manager.run()
